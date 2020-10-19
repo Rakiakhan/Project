da8a39268a76f6bcd5f631dcd3f5d8cf7c38a45d
rakia khan <rakia.khan56@gmail.com>
2020-10-19 10:05:46 -0700

    project

---
+++ js/firebase-init.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyD-7F3Mld-u3844PChmJc07ngQ0zafXjLo",
+    authDomain: "chat-application-2811b.firebaseapp.com",
+    databaseURL: "https://chat-application-2811b.firebaseio.com",
+    projectId: "chat-application-2811b",
+    storageBucket: "chat-application-2811b.appspot.com",
+    messagingSenderId: "334469250133",
+    appId: "1:334469250133:web:c54ae4afe87aec5567a8dc",
+    measurementId: "G-3Y025CHCZG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
